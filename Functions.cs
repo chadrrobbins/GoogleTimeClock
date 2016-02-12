fcf7d4bed465531f498594f308c1d71baf9a8181
chadrrobbins <chad.r.robbins@gmail.com>
2016-02-11 19:15:03 -0700

    Initial Commit

---
+++ Functions.cs
@@ -0,0 +1,107 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoogleTimeClock
+{
+    class Functions
+    {
+        //static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        //static string ApplicationName = "Google Calendar API .NET Quickstart";
+
+        public void OpenFileStream(out UserCredential credential, string[] Scopes)
+        {            
+            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/calendar-dotnet-quickstart");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.Load(stream).Secrets, Scopes, "user", CancellationToken.None, new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+        }
+
+        //public void CreateGoogleService(UserCredential credential, string ApplicationName)
+        //{
+        //    var service = new CalendarService(new BaseClientService.Initializer()
+        //    {
+        //        HttpClientInitializer = credential,
+        //        ApplicationName = ApplicationName,
+        //    });
+        //}        
+
+        public void RunAppMenu()
+        {
+            
+            Console.WriteLine("Select task to perform.");
+            Console.WriteLine("1. Calculate employee hours.");
+            Console.WriteLine("2. Exit.");
+        }    
+
+        public string GetTaskSelection()
+        {
+            string task;
+
+            task = Console.ReadLine();
+            ValidateTask(task);
+
+            return task;
+        }
+
+        public int ValidateTask(string Task)
+        {
+            int isValid = 0;
+            int value;
+
+            while (isValid == 0)
+            {
+                if (int.TryParse(Task, out value) && value >= 1 && value <= 2)
+                {
+                    isValid = 1;
+                }
+                else
+                {                    
+                    Console.Clear();
+                    Console.WriteLine("Please enter a number between 0 and 2.");
+                    RunAppMenu();
+                    GetTaskSelection();
+                }
+            }
+
+            return isValid;
+        }
+
+        public void GetDateRange(out string startDate, out string endDate)
+        {            
+            Console.WriteLine("Please enter the starting date to use to calculate employee hours in the format YYYY-MM-DD.");
+            startDate = Console.ReadLine();
+            Console.WriteLine("Please enter the end date to use to calculate employee hours in the format YYYY-MM-DD.");
+            endDate = Console.ReadLine();
+        }        
+
+        public void ExecuteTask(string Task)
+        {
+            switch (Convert.ToInt32(Task))
+            {
+                case 1:
+                    //Display hours for all employees for two week time period.
+                    
+                    return;
+                case 2:
+                    Environment.Exit(0);
+                    return;
+            }
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CalendarQuickstart
+{
+    class Program
+    {
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "Google Calendar API .NET Quickstart";
+
+
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+            var Functions = new GoogleTimeClock.Functions();
+            //var service = new CalendarService(new BaseClientService.Initializer());
+
+            //Open file IO stream.
+            Functions.OpenFileStream(out credential, Scopes);
+            
+
+            // Create Google Calendar API service.
+            //Functions.CreateGoogleService(credential, ApplicationName);
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            //Display application menu            
+            string task;
+
+            Functions.RunAppMenu();
+            task = Functions.GetTaskSelection();
+            Functions.ExecuteTask(task);
+            //appMenu.ValidateTask(task);           
+                                    
+            //Evaluate task selection
+
+
+            // Define parameters of request.
+            string startDate;
+            string endDate;
+            Functions.GetDateRange(out startDate, out endDate);            
+            EventsResource.ListRequest request = service.Events.List("mba2940@gmail.com");
+            request.TimeMin = DateTime.ParseExact(startDate, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+            request.TimeMax = DateTime.ParseExact(endDate, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 20;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            // List events.
+            Events events = request.Execute();
+            Console.WriteLine("Upcoming events:");
+            if (events.Items != null && events.Items.Count > 0)
+            {
+                foreach (var eventItem in events.Items)
+                {
+                    string when = eventItem.Start.DateTime.ToString();
+                    //DateTime whenDate = Convert.ToDateTime(when);
+                    //DateTime whenDate = DateTime.ParseExact(when, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+                    string eventStart = eventItem.Start.DateTime.ToString();
+                    //DateTime startTime = Convert.ToDateTime(eventStart);
+                    //DateTime startTime = DateTime.ParseExact(when, "hh:mm tt", System.Globalization.CultureInfo.InvariantCulture);
+                    string eventEnd = eventItem.End.DateTime.ToString();
+                    //DateTime endTime = Convert.ToDateTime(eventEnd);
+                    //DateTime endTime = DateTime.ParseExact(when, "hh:mm tt", System.Globalization.CultureInfo.InvariantCulture);
+                    if (string.IsNullOrEmpty(when))
+                    {
+                        //when = eventItem.Start.Date;
+                    }
+                    //Console.WriteLine("{0} {1} {2} {3}", eventItem.Summary + "\r\n", whenDate.ToString("MM/DD/YYYY") + "\r\n", startTime.ToString("hh:mm tt") + "\r\n", endTime.ToString("hh:mm tt"));
+                    Console.WriteLine("{0} {1} {2}", eventItem.Summary + "\r\n", eventStart.ToString() + "\r\n", eventEnd.ToString() + "\r\n");
+                    Console.WriteLine("Total: " + (eventItem.End.DateTime - eventItem.Start.DateTime) + "\r\n");                }
+            }
+            else
+            {
+                Console.WriteLine("No upcoming events found.");
+            }
+            Console.Read();
+
+        }
+    }
+}
