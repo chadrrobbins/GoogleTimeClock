84382b0b5f54993dcd7197baf39e8413a597e07e
chadrrobbins <chad.r.robbins@gmail.com>
2016-02-14 18:29:44 -0700

    Created Job and Employee classes.  Job class contains information about the construction company, job title, job location, and total labor hours.  The Employee class contains information about the employee's first name, last name, and the total hours for the define pay period.  May need to remove the pay period hours field, and possibly create a PayPeriod class?

---
+++ Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleTimeClock
+{
+    class Employee
+    {
+        private String firstName;
+        private String lastName;
+        private int totalHours;
+
+        public Employee(String _firstName, String _lastName, int _totalHours)
+        {
+            this.firstName = _firstName;
+            this.lastName = _lastName;
+            this.totalHours = _totalHours;
+        }
+
+        public String FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                this.firstName = value;
+            }
+        }
+
+        public String LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                this.lastName = value;
+            }
+        }
+
+        public int TotalHours
+        {
+            get
+            {
+                return this.totalHours;
+            }
+            set
+            {
+                this.totalHours = value;
+            }
+        }
+    }
+}
---
+++ Job.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleTimeClock
+{
+    class Job
+    {
+        private String contractorName;
+        private String jobTitle;
+        private String jobLocation;
+        private int totalHours;
+
+        public Job(String _contractorName, String _jobTitle, String _jobLocation, int _totalHours)
+        {
+            this.contractorName = _contractorName;
+            this.jobTitle = _jobTitle;
+            this.jobLocation = _jobLocation;
+            this.totalHours = _totalHours;
+        }
+
+        public String ContractorName
+        {
+            get
+            {
+                return this.contractorName;
+            }
+            set
+            {
+                this.contractorName = value;
+            }
+        }
+
+        public String JobTitle
+        {
+            get
+            {
+                return this.jobTitle;
+            }
+            set
+            {
+                this.jobTitle = value;
+            }
+        }
+
+        public String JobLocation
+        {
+            get
+            {
+                return this.jobLocation;
+            }
+            set
+            {
+                this.jobLocation = value;
+            }
+        }
+
+        public int TotalHours
+        {
+            get
+            {
+                return this.totalHours;
+            }
+            set
+            {
+                this.totalHours = value;
+            }
+        }
+    }
+}
